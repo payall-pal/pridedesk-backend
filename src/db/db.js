cfac09551a34c0eba552a4ee29299ef769ef806a
payal <payall01261@gmail.com>
2026-04-03 14:00:54 +0530

    .env corrected

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose")
 
-function connectDB(){
-    mongoose.connect(process.env.MONGODB_URI)
-    
+async function connectDB() {
 
-    console.log("Connected to DB")
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connected to DB")
+    } catch(error){
+        console.log("DB connection error")
+    }
+   
+
 }
 
 module.exports = connectDB
