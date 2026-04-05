70733d3d4fcd58e68ed8e182414142c31550ac28
payal <payall01261@gmail.com>
2026-04-05 13:12:12 +0530

    Updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB()
 
 
 
-// const PORT = process.env.PORT || 3000
-app.listen(3000, ()=>{
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>{
     console.log("server is running on port 3000")
 })
